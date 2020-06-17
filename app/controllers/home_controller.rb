68952dd73591a798114c94f1bf3470baa785b378
gnanesh007.mk@gmail.com <gnanesh007.mk@gmail.com>
2020-06-17 14:11:32 +0530

    about us page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   skip_before_action :ensure_user_logged_in
 
   def index
-    current_user
     render "index"
+  end
+
+  def about
+    render "about"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/" => "home#index"
+  get "/about" => "home#about"
   resources :menu_items
   resources :orders
   resources :users
