1a95d3187c5de1b563192bbed06d6765583adf1a
gnanesh007.mk@gmail.com <gnanesh007.mk@gmail.com>
2020-06-21 12:35:15 +0530

    view user orders

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -17,12 +17,15 @@ class CustomersController < ApplicationController
                                    to_date: to_date }
   end
 
-  def show_report
-    date1 = params[:from_date]
-    date2 = params[:to_date]
-    @reports = Order.getorders(date1, date2)
-    redirect_to report_path
+  def show
+    id = params[:id]
+    @user = User.find(id)
+    if @user.role == "user"
+      @orders = Order.user_delivered(@user)
+    else
+      @walk_in_customer = walk_in_customer
+      @orders = Order.user_delivered(@walk_in_customer)
+    end
+    render "show"
   end
-
-  
 end
